594426a4965285d0f5ebdfc3877417e565708133
mdaamer250 <mohammed.aamer@aurainsure.tech>
2025-09-08 16:59:55 +0530

    Fix: Resource Page

--- src/api/resource-page/controllers/resource-page.ts
+++ src/api/resource-page/controllers/resource-page.ts
@@ -21,72 +21,11 @@ export default factories.createCoreController(
 					},
 					featuredArticlesSection: true,
 					recentArticlesSection: true,
-					categoryFilter: {
-						populate: {
-							categories: {
-								populate: {
-									category: {
-										fields: ["id", "name"],
-									},
-								},
-							},
-						},
-					},
 					seo: true,
 				},
 			};
 
-			const resourcePage = await super.find(ctx);
-
-			// Fetch featured articles
-			const featuredArticles = await strapi.entityService.findMany(
-				"api::article.article",
-				{
-					filters: {
-						featuredInResourcePage: true,
-					},
-					populate: {
-						cover: {
-							fields: ["id", "alternativeText", "url"],
-						},
-						author: {
-							fields: ["id", "name"],
-						},
-						category: {
-							fields: ["id", "name"],
-						},
-					},
-				}
-			);
-
-			// Fetch recent articles
-			const recentArticles = await strapi.entityService.findMany(
-				"api::article.article",
-				{
-					filters: {
-						recentInResourcePage: true,
-					},
-					populate: {
-						cover: {
-							fields: ["id", "alternativeText", "url"],
-						},
-						author: {
-							fields: ["id", "name"],
-						},
-						category: {
-							fields: ["id", "name"],
-						},
-					},
-				}
-			);
-
-			// Add the articles to the response
-			if (resourcePage.data) {
-				resourcePage.data.featuredArticles = featuredArticles;
-				resourcePage.data.recentArticles = recentArticles;
-			}
-
-			return resourcePage;
+			return await super.find(ctx);
 		},
 	})
 );
