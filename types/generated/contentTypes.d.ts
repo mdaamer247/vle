2049be8787aaebf480e3297139bbf453bfde5fd9
mdaamer250 <mohammed.aamer@aurainsure.tech>
2025-11-26 16:18:12 +0530

    Add: New Hero Highlight Card Component and Update Hero Section

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -484,6 +484,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    videoUrl: Schema.Attribute.String;
   };
 }
 
